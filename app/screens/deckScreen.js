42a92926b2081f2902747743150bb34697b8f28e
Aaron Humerickhouse <aaron.humerickhouse@bigcommerce.com>
2019-06-01 00:38:22 -0500

    Deck screen components

--- app/screens/deckScreen.js
+++ app/screens/deckScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet } from 'react-native';
-import { Text, Container } from 'native-base'
+import { Button, Container, H1, Text, View } from 'native-base'
 import {connect} from 'react-redux'
 
 class DeckScreen extends React.Component {
@@ -10,11 +10,29 @@ class DeckScreen extends React.Component {
     };
   };
 
+  questionLength = () => {
+    const {deck} = this.props
+    return deck.questions.length
+  }
+
   render() {
     const {deck} = this.props
     return(
-      <Container>
-        <Text>This is a deck with id {deck.title}</Text>
+      <Container style={styles.container}>
+        <View style={[styles.centerText, styles.padded]}>
+          <H1>{deck.title}</H1>
+          <Text>
+            {this.questionLength()} card{this.questionLength() > 1 ? 's' : ''}
+          </Text>
+        </View>
+        <View style={styles.padded}>
+          <Button light style={styles.button}>
+            <Text>Add Card</Text>
+          </Button>
+          <Button primary style={styles.button}>
+            <Text>Start Quiz</Text>
+          </Button>
+        </View>
       </Container>
     )
   }
@@ -24,7 +42,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'stretch',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  centerText: {
+    alignItems: 'center',
+  },
+  padded: {
+    padding: 50
+  },
+  button: {
+    margin: 5
   }
 })
 
